2a13fb5bbddd47fbd59e7c07d153a2cef5fc508e
Kristiano <kristianokasper@gmail.com>
2021-07-27 23:39:38 -0300

    Adicionando dinamicidade ao teste

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,9 +41,13 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(Object.values(productDetails('Test1', 'Test2')[0]), Object.values(productDetails('Test1', 'Test2')[1]));
     // Teste que os dois productIds terminam com 123.
-    let x = productDetails('Test1', 'Test2')[0].details.productId;
-    let y = productDetails('Test1', 'Test2')[1].details.productId;
-    assert.strictEqual(parseInt(x.substring(x.length - 3)), 123);
-    assert.strictEqual(parseInt(y.substring(y.length - 3)), 123);
+    // let x = productDetails('Test1', 'Test2')[0].details.productId;
+    // let y = productDetails('Test1', 'Test2')[1].details.productId;
+    // assert.strictEqual(parseInt(x.substring(x.length - 3)), 123);
+    // assert.strictEqual(parseInt(y.substring(y.length - 3)), 123);
+    const pD = productDetails('Test1', 'Test2');
+    for (let i = 0; i < pD.length; i += 1) {
+      assert.strictEqual(parseInt(pD[i].details.productId.substring(pD[i].details.productId.length - 3)), 123);
+    }
   });
 });
